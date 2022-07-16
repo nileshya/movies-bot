bdc1cf68bb994401009dacac3605b82669260cda
nileshya <72087983+nileshya@users.noreply.github.com>
2022-07-16 11:33:56 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -54,7 +54,7 @@
      BANNED_USERS = [] 
      ME = None # User Id 
      Bot_Username = None # Username 
-     Bot_Name = "LuciferMoringstar" # Full Name  
+     Bot_Name = "CharlieBot" # Full Name  
      BUTTONS = {} # AutoFilter 
      CURRENT = int(environ.get("SKIP", 2)) # Skip Files 
      CANCEL = False # Cancel Index 
