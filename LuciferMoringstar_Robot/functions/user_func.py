c0143c2859e36cee51e68adf08bf2d83829f3586
nileshya <72087983+nileshya@users.noreply.github.com>
2022-07-16 13:51:30 +0530

    Update user_func.py

--- LuciferMoringstar_Robot/functions/user_func.py
+++ LuciferMoringstar_Robot/functions/user_func.py
@@ -1,6 +1,6 @@
 # MIT License 
   
- # Copyright (c) 2022 Muhammed 
+ # Copyright (c) 2022 Charlie
   
  # Permission is hereby granted, free of charge, to any person obtaining a copy 
  # of this software and associated documentation files (the "Software"), to deal 
@@ -12,17 +12,19 @@
  # The above copyright notice and this permission notice shall be included in all 
  # copies or substantial portions of the Software. 
   
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
  # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
  # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
  # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
  # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
  # SOFTWARE. 
   
- # Telegram Link : https://telegram.dog/Mo_Tech_Group 
- # Repo Link : https://github.com/PR0FESS0R-99/LuciferMoringstar-Robot 
- # License Link : https://github.com/PR0FESS0R-99/LuciferMoringstar-Robot/blob/LuciferMoringstar-Robot/LICENSE 
+# Telegram Link : https://telegram.dog/HO_Tech_Group 
+
+ # Repo Link : https://github.com/nileshya/movies-bot 
+
+ # License Link : https://github.com/nileshya/movies-bot/blob/main/LICENSE 
    
  from pyrogram.types import Message 
  from typing import Union 
